2f9b99855eb7497d68332ca6d82e3ed82112d8cc
gjjoo <joogeunjae@gmail.com>
2023-02-18 17:00:55 +0900

    chore: 자주 방문하는 링크 대시보드에 등록

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -12,7 +12,44 @@ const Dashboard = () => {
           <Title>Welcome to Escape Note</Title>
         </Box>
         <Box>
-          ...
+          <ul>
+            <li>
+              <a
+                href="https://analytics.google.com/analytics/web/?authuser=3#/p345989485/reports/reportinghub?params=_u..nav%3Dmaui%26_u.dateOption%3Dlast28Days%26_u.comparisonOption%3Ddisabled"
+                target="_blank"
+                rel="noreferrer"
+              >
+                📈 Google Analytics
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://search.google.com/u/3/search-console?resource_id=sc-domain%3Aescape-note.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                🔍 Google Search Console
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://map.naver.com/v5/entry/place/1716448056"
+                target="_blank"
+                rel="noreferrer"
+              >
+                🗺 Find naver map place id
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://m.place.naver.com/place/1429097769/home"
+                target="_blank"
+                rel="noreferrer"
+              >
+                🏡 Naver place
+              </a>
+            </li>
+          </ul>
         </Box>
       </Section>
     </Box>
